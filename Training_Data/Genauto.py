1fffe6d70a8fadf071d849264ffff7d5b501c90e
firdauzfan <firdauzfanani@gmail.com>
2018-09-27 15:51:24 +0700

    tambah dataset dan fix bugs

--- Training_Data/Genauto.py
+++ Training_Data/Genauto.py
@@ -31,18 +31,19 @@ def main():
         x=0
         path = os.path.join(training_dataset_dir, each_letter)
         for files in os.listdir(path):
-            if files.endswith('.jpg'):
+            if files.endswith('.png'):
                 x+=1
-            for each in range(x):
-                #training_data[1] is for 10X20 training data images
-                img_details = imread(training_dataset_dir+'/'+each_letter+'/'+each_letter+'_'+str(each)+'.jpg', as_grey=True)
-                imgROIResized = cv2.resize(img_details, (RESIZED_IMAGE_WIDTH, RESIZED_IMAGE_HEIGHT))
-                cv2.imwrite("coba.png",imgROIResized)
+        for each in range(x):
+            #training_data[1] is for 10X20 training data images
+            img_details = imread(training_dataset_dir+'/'+each_letter+'/'+each_letter+'_'+str(each)+'.png', as_grey=True)
+            imgROIResized = cv2.resize(img_details, (RESIZED_IMAGE_WIDTH, RESIZED_IMAGE_HEIGHT))
 
-                intLetter= ord(each_letter)
-                intClassifications.append(intLetter)
-                npaFlattenedImage = imgROIResized.reshape((1, RESIZED_IMAGE_WIDTH * RESIZED_IMAGE_HEIGHT))  # flatten image to 1d numpy array so we can write to file later
-                npaFlattenedImages = np.append(npaFlattenedImages, npaFlattenedImage, 0)
+            print("%s %s" %(each_letter,each))
+
+            intLetter= ord(each_letter)
+            intClassifications.append(intLetter)
+            npaFlattenedImage = imgROIResized.reshape((1, RESIZED_IMAGE_WIDTH * RESIZED_IMAGE_HEIGHT))  # flatten image to 1d numpy array so we can write to file later
+            npaFlattenedImages = np.append(npaFlattenedImages, npaFlattenedImage, 0)
 
     fltClassifications = np.array(intClassifications, np.float32)                   # convert classifications list of ints to numpy array of floats
 
